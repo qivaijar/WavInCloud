6dd2e2ed36fb29497799a70dbcae48d7ee1944a5
qivaijar <qivaijar@gmail.com>
2023-12-28 18:17:02 +0000

    Refactored db-related scripts

--- back-end/database/db_models.py
+++ back-end/database/db_models.py
@@ -3,7 +3,8 @@ from uuid import UUID, uuid4
 
 from sqlmodel import Field, SQLModel
 
-class AudioInfo(SQLModel, table=True):
+
+class AudioData(SQLModel, table=True):
     file_id: UUID = Field(default=uuid4, primary_key=True)
     title: str = Field(index=True, unique=True)
     artist: str = Field(index=True)
@@ -11,4 +12,3 @@ class AudioInfo(SQLModel, table=True):
     lyric: Optional[str] = None
     file_path: str
     added_at: str
-    
