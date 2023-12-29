08721bb57342b98c943de073f1e940919f4d19b3
qivaijar <qivaijar@gmail.com>
2023-12-29 18:53:00 +0000

    Added alternative title column and set file_path to be unique

--- back-end/database/db_models.py
+++ back-end/database/db_models.py
@@ -7,8 +7,9 @@ from sqlmodel import Field, SQLModel
 class AudioData(SQLModel, table=True):
     file_id: UUID = Field(default=uuid4, primary_key=True)
     title: str = Field(index=True, unique=True)
+    alternative_title: Optional[str] = Field(index=True, unique=True)
     artist: str = Field(index=True)
     genre: Optional[str] = None
     lyric: Optional[str] = None
-    file_path: str
+    file_path: str = Field(unique=True)
     added_at: str
